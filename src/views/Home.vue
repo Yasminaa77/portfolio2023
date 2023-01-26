455184cbab1320993184529c777df6140812bf3c
Yasmina Amir <66489192+Yasminaa77@users.noreply.github.com>
2023-01-25 16:15:20 -0800

    Init submit

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<script setup>
+//import Card from '../components/home/Card.vue';
+
+</script>
+
+<template>
+  <div class="development">
+  <div class="center" >
+    <img src="../assets/logo_dark.png" alt="hero-image" width="50" height="60" >
+    <h2>Under Development</h2>
+    <p> A cool portfolio is getting developed </p>
+  </div>
+  </div>
+
+
+</template>
+
+<script>
+export default {
+  name: "Home",
+  methods: {
+
+  },
+  computed: {},
+  props: {},
+  data() {
+    return {
+
+    };
+  },
+  components: {
+  }
+};
+</script>
+
+<style>
+
+.development{
+  display: flex;
+  padding:20px;
+}
+
+.center{
+  text-align: center;
+  align-items: center;
+  justify-content: center;
+}
+
+</style>
